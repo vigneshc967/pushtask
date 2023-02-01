bf2ea7f1e0f8017721a74f994e403406f6b9b758
vigneshc967 <vigneshc967@gmail.com>
2023-02-01 08:31:25 +0530

    task2

---
+++ src/org/test/Git.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Git {
+	private void employeeid() {
+		// TODO Auto-generated method stub
+   System.out.println();
+	}
+private void employeename() {
+	// TODO Auto-generated method stub
+System.out.println();
+}
+}
